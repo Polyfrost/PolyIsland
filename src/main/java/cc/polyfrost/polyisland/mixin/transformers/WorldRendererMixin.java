b14e97db15e29df19bc010c9d0a9117b00da74ad
Wyvest <45589059+Wyvest@users.noreply.github.com>
2022-09-24 20:00:15 -0400

    oops

--- src/main/java/cc/polyfrost/polyisland/mixin/transformers/WorldRendererMixin.java
+++ src/main/java/cc/polyfrost/polyisland/mixin/transformers/WorldRendererMixin.java
@@ -4,7 +4,11 @@ import cc.polyfrost.polyisland.config.ConfigManager;
 import cc.polyfrost.polyisland.game.Game;
 import cc.polyfrost.polyisland.game.GameState;
 import cc.polyfrost.polyisland.game.GameTracker;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,8 +17,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
     @Inject(method = "renderEntity", at = @At("HEAD"), cancellable = true)
-    private void onRenderEntity(CallbackInfo ci) {
-        if (ConfigManager.INSTANCE.getConfig().hideHITWPlayers && GameTracker.INSTANCE.getGame() == Game.HOLE_IN_THE_WALL && GameTracker.INSTANCE.getState() == GameState.ACTIVE) {
+    private void onRenderEntity(Entity entity, double cameraX, double cameraY, double cameraZ, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo ci) {
+        if (ConfigManager.INSTANCE.getConfig().hideHITWPlayers && GameTracker.INSTANCE.getGame() == Game.HOLE_IN_THE_WALL && GameTracker.INSTANCE.getState() == GameState.ACTIVE && entity != MinecraftClient.getInstance().player) {
             ci.cancel();
         }
     }
